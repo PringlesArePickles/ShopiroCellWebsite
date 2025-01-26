07587c9359502baa69a2ba0b48f15cf6142492f1
Kushal patil <kushalpatil@laptopbqhy.mynetworksettings.com>
2025-01-26 11:17:56 -0500

    added a nav bar and made the quiz thingy

--- Website/app.py
+++ Website/app.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+@app.route("/quiz", methods = ["GET", "POST"])
+def quiz():
+    if request.method == "POST":
+        print("OK")
+    elif request.method == "GET":
+        print("OK")
+        return render_template("quiz.html")
 
 if __name__ == "__main__":
     app.run()
